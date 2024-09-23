2d8707935bb51ff9e8ce2855b857a02559f00a00
obafisoye <dannieoba@gmail.com>
2024-09-23 08:35:55 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/moviehubproject/screens/MovieDetailScreen.kt
@@ -0,0 +1,11 @@
+package com.example.moviehubproject.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun MovieDetailScreen(modifier: Modifier = Modifier){
+    Text(text = "Movie Detail Screen")
+}
