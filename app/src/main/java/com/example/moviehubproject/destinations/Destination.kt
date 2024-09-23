58fe455de65629422bd0b65eed096e2320e4ada2
obafisoye <dannieoba@gmail.com>
2024-09-23 08:54:49 -0500

    Updated

--- app/src/main/java/com/example/moviehubproject/destinations/Destination.kt
+++ app/src/main/java/com/example/moviehubproject/destinations/Destination.kt
@@ -1,2 +1,15 @@
 package com.example.moviehubproject.destinations
 
+/*
+ This class is responsible for where the app should navigate to.
+ By using sealed classes, we are not able to further subclass this class.
+ */
+
+sealed class Destination(val route: String){
+    object Movie: Destination("movie")
+    object Search: Destination("search")
+    object Favoriite: Destination("favorite")
+    object MovieDetail: Destination("movieDetail/{movieId}"){
+        fun createRoute(movieId: Int?) = "movieDetail/$movieId"
+    }
+}
